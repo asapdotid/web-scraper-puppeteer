115f0ae3e55295af71d026867349c5da0004e50d
Agung S Saputra <asapdotid@gmail.com>
2018-08-16 15:33:20 +0700

    delete file not use

--- website-scraper.js
+++
@@ -1,9 +0,0 @@
-let scrape = require('website-scraper');
-
-scrape({
-  urls: ['https://coursehunters.net/course/ultimate-laravel-kurs-2018-paypal-webshop-restful-api'],
-  directory: './coursehunters',
-  subdirectories: [
-    {directory: 'video', extensions: ['.mp4']}
-  ]
-}).then(console.log).catch(console.log);
