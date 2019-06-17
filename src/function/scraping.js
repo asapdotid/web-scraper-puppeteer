66bf8b959757bf38e121e4a8c86020943705d593
Agung S Saputra <asapdotid@gmail.com>
2019-06-17 18:14:59 +0700

    update babel v7, update script for scrub

--- src/function/scraping.js
+++ src/function/scraping.js
@@ -16,25 +16,27 @@ exports.Scraping = async () => {
 	    let r = []
 	    let data= []
 
-	    let lessonTitleElement = document.querySelector('div.original-name')
+	    let lessonTitleElement = document.querySelector('p.hero-description')
 		let lessonTitle = lessonTitleElement.textContent || lessonTitleElement.innerText
 		let lessonTitleClean = lessonTitle.replace(/[^a-zA-Z0-9-. ]/g, "")
 
-	    let elements = document.querySelectorAll('li.lessons-list__li') // Select all Files
+	    let elements = document.querySelectorAll('li.lessons-item') // Select all Files
 		
-		elements.forEach( (element) => {
-			let str = element.childNodes[3].innerText // Select the title
-	        let strArry = str.split(" ")
-	        strArry[0] = 'lesson'
-			let title = strArry.join(' ')
+		elements.forEach( (element, index) => {
+			// let str = element.childNodes[1].innerText // Select the title
+	        // let strArry = str.split(" ")
+	        // strArry[0] = 'lesson'
+			// let title = strArry.join(' ')
+			let number = index++
+			let title = 'Lesson ' + number + ' - ' + element.querySelector("div[class='lessons-name']").innerText
 	        let url = element.querySelector("link[itemprop='contentUrl']").href // Select the url link
 
 	        data.push({title, url}) // Push an object with the data onto our array	
 		})
 
 	    r[0] = lessonTitleClean
-	    r[1] = data
-
+		r[1] = data
+		
 	    return r
 	});
 
